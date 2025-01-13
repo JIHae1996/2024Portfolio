49e1b563465e5d96569969c216b8230896559df4
jihae96 <113869780+jihae96@users.noreply.github.com>
2025-01-13 21:53:44 +0900

    Updates

--- src/data/loadIcons.js
+++
@@ -1,8 +0,0 @@
-const requireContext = require.context('../assets/nav_ico', false, /\.(png)$/);
-const icons = requireContext.keys().reduce((acc, file) => {
-  const key = file.replace('./', '').replace('.png', '');
-  acc[key] = requireContext(file);
-  return acc;
-}, {});
-
-export default icons;
